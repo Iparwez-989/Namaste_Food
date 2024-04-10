38002287486b1a2b2c81c2ab4bd1f693b004270a
Irfan Parwez <136509687+Iparwez-989@users.noreply.github.com>
2024-04-10 23:35:37 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,7 @@
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
-
+//This url is for uploading images
 export const LOGO_URL = "https://static.vecteezy.com/system/resources/previews/008/687/818/original/food-delivery-logo-free-vector.jpg"
+// This url is for logo of the page
 
 export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.37240&lng=78.43780&restaurantId="
+// This url is fetches the menu from swiggy
