d39b85f9ea07f89368d7942f9ea5c197c46feb0d
Irfan Parwez <iparwez351412@gmail.com>
2024-04-17 22:13:42 +0530

    Using custom hooks

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,43 +1,11 @@
-import React from "react";
-import { useState,useEffect } from "react";
-import { useParams } from "react-router-dom";
-import Shimmer from "./Shimmer";
-import {MENU_API} from "../utils/constants"
-const RestaurantMenu = ()=>{
-const [resMenu,setResMenu]= useState('')
-const {resId} = useParams();
-console.log(resId)
-useEffect(()=>{
-    fetchMenu()
-},[])
-    const fetchMenu=async ()=>{
-        const data = await fetch(MENU_API+resId)
-        const json = await data.json()
-        setResMenu(json.data)
-        console.log(json)
-    }
-// Shimmer component will show before the data is fetched
-if (resMenu=== '') return <Shimmer/>
+import React from 'react'
 
-const {itemCards}=resMenu?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
-console.log(itemCards)
-    return(
-        <div>
-            <h1>{resMenu?.cards[0]?.card?.card?.info.name}</h1>
-            <h3>{resMenu?.cards[0]?.card?.card?.info.areaName}</h3>
-            <p>{resMenu?.cards[0]?.card?.card?.info.cuisines.join(', ')} - {resMenu?.cards[0]?.card?.card?.info.costForTwoMessage}</p>
-            <h1>Menu</h1>
-            <ol type="1">
-                {/* <li>{itemCards[0].card.info.name}</li>
-                <li>{itemCards[1].card.info.name}</li>
-                <li>{itemCards[2].card.info.name}</li> */}
-                {itemCards.map((item )=><li key={item.card.info.id}>{ item.card.info.name}- Rs {item.card.info.price/100}</li>
-                 )}
-            </ol>
-        </div>
-    )
-
+const RestaurantMenu = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  )
 }
 
-
-export default RestaurantMenu;
+export default RestaurantMenu
